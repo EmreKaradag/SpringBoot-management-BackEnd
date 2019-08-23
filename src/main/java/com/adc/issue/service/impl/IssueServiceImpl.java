10f6aee7168ed2bff421b28946eda79958a7de09
Emre <50354587+AliEmreKARADAG@users.noreply.github.com>
2019-08-23 09:07:42 +0300

    Update IssueServiceImpl.java

--- src/main/java/com/adc/issue/service/impl/IssueServiceImpl.java
+++ src/main/java/com/adc/issue/service/impl/IssueServiceImpl.java
@@ -14,10 +14,6 @@ import java.util.Arrays;
 
 public class IssueServiceImpl implements IssueService {
 
- //imp save işlemini bu entitynin repositorysi üzerinden gerçeklleştirir. bu repositoryi inject etmemmız lazım
-  // constructor injektion yapcaz. bir nesneye bir property e final etiketi verdiğiniz zaman bu nesnenın run time de değişmesini engellemiş baska birinin birşey atamasını engellemiş oluyoruz.
-  // final bir nesneye atamayı sadece constructor içerisinde yapabiliyoruz
-
 
   private final IssueRepository issueRepository;
   private final ModelMapper modelMapper;
@@ -38,9 +34,7 @@ public class IssueServiceImpl implements IssueService {
    Issue issueDb= modelMapper.map(issue,Issue.class);
     issueDb=issueRepository.save(issueDb);
    return  modelMapper.map(issueDb,IssueDto.class);
-   //önce dışardan gelen nesneyi(dto) içerdeki modele döndür. içerdeki modelden de veri tabnanına kaydettıkten sonra
-    //o nesneyi yine dto ya dönüştürüp geri ver.
-
+ 
   }
 
   @Override
@@ -50,8 +44,7 @@ public class IssueServiceImpl implements IssueService {
 
   @Override
   public TPage<IssueDto> getAllPageable(Pageable pageable) {
-    Page<Issue> data = issueRepository.findAll(pageable);//pageble olarak bütün kayıtları Issueları çektim.Amacım dto ya dönüştürüp geri vermek.
-    //Dönüşüm.Dönüşümü kendı response nesnemle yapıyorum TPage.
+    Page<Issue> data = issueRepository.findAll(pageable);
     TPage page=new TPage<IssueDto>();
    IssueDto[] dtos= modelMapper.map(data.getContent(),IssueDto[].class);
     page.setStat(data, Arrays.asList(dtos));
