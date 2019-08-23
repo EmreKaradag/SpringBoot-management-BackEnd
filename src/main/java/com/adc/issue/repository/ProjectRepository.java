35c62be0a8af0f6838c89fd24f8c157fc08767dc
Emre <50354587+AliEmreKARADAG@users.noreply.github.com>
2019-08-23 09:06:27 +0300

    Update ProjectRepository.java

--- src/main/java/com/adc/issue/repository/ProjectRepository.java
+++ src/main/java/com/adc/issue/repository/ProjectRepository.java
@@ -12,17 +12,11 @@ import java.util.List;
 public interface ProjectRepository extends JpaRepository<Project,Long> {
 
   List<Project> getAllByProjectCode(String projectCode);
-  //Ben project codunu vereyim oda bana projectCode ile eşleşen bütün kayıtları getirsin
-//string kısmına direk property ismini yazıyoruz
-  //veritabanında bir select işlemi yapmış oluyoruz
 
   List<Project>getAllByProjectCodeAndIdNotNull(String projectCode);
-  //Id si null olmayacak ve ProjectCode da projectCode olan kayıtlar gelsın
-
+ 
 
   List<Project> getAllByProjectCodeAndProjectNameContains(String projectCode,String name);
-
-//bu repositoryleri singelten instance olarak kullanacaz uygulamamızda
 
 
   Page<Project> findAll(Pageable pageable);
