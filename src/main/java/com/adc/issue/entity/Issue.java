4e8c35fab99a356135f51ca980477262a8c00a4e
Emre <50354587+AliEmreKARADAG@users.noreply.github.com>
2019-08-23 09:04:44 +0300

    Update Issue.java

--- src/main/java/com/adc/issue/entity/Issue.java
+++ src/main/java/com/adc/issue/entity/Issue.java
@@ -15,7 +15,7 @@ import java.util.Date;
 public class Issue extends BaseEntity{
 
   @Id//PrimaryKey
-  @GeneratedValue(strategy = GenerationType.AUTO)//Sistem tarafından oto olusturmasını saglıyoruz.id nin oto artması
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
   @Column(name = "description",length = 1000)
@@ -29,7 +29,7 @@ public class Issue extends BaseEntity{
   private Date date;
 
   @Column(name = "issue_status")
-  @Enumerated(EnumType.STRING) //veritabanında bu verinin nasıl durmasını ıstıyorsunuz
+  @Enumerated(EnumType.STRING) 
   private IssueStatus issueStatus;
 
   @JoinColumn(name = "assignee_user_id")
@@ -40,15 +40,6 @@ public class Issue extends BaseEntity{
   @ManyToOne(optional = true,fetch = FetchType.LAZY)
   private Project project;
 
-  //ıssue  assignee manytoone ile bi join işlemi yaptık. Bir tablodan diğer tabloya
-  //bi relation tanımı yapıyorsak ilgili prop içinde veya  ilgili tablolar birbiri içerisinde ilişki tipine göre prop olarak yaratılır
-  //many ilk etiket yani birçok issue class ı birtane user baglanabilir
-  //biri optional true yani bi issue nesnesi yaratırken bunun assignee yani bir kullanıcıya atamasanda olur
-  //fetch ise , burada bu issue tablosuna select atılırken. User tablosundaki ilişkili tablosuna join yapıp getirilsinmi
-  //lazy bu methodun sadece getter çagırıldıgında yanı sadece ıhtiyac oldugunda cagırılır
-  //eager veritabanına yük getirir.performans kaybı olur.
-
-
-  //ZOrunlı olan 2 durum var .1 si PrimaryKey. 2.si Entity annotationu
+ 
 
 }
